addd158b776f86ebc43f3e22ff8669f71dd9960e
ivringliu <ivringliu@tencent.com>
2017-01-11 20:19:20 +0800

    初始化工程

---
+++ lyxTool/lyxTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lyxTool
+//
+//  Created by liuyunxuan on 2017/1/11.
+//  Copyright © 2017年 liuyunxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lyxTool/lyxTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lyxTool
+//
+//  Created by liuyunxuan on 2017/1/11.
+//  Copyright © 2017年 liuyunxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
